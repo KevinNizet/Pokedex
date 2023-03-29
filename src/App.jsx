bee0dcb7468841c28d397bc793cea869a2937eab
KevinNizet <kevin.nizet@gmail.com>
2023-03-29 16:30:52 +0200

    ajout des propTypes pour la pokemonCard

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
+
 function App() {
 
   const pokemonList = [
     {
-      name: "bulbasaur",
+      name: "Bulbizarre",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "mew",
+      name: "Mew",
     },
   ];
 
