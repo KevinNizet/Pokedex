2669abaa5d4e5b5b6207e5e1c69e0bd816f78ace
KevinNizet <kevin.nizet@gmail.com>
2023-03-29 11:59:01 +0200

    le composant pokemon card attend maintenant une prop pokemon. Le composant App envoie les données d'un pokémon au composant PokemonCard via la prop pokemon.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,14 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({pokemon}) {
 
   return (
+    <> 
     <figure>
-    
-      {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} />}
-
+      {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name}/>}
     </figure>
+
+    <p> Hello I'm {pokemon.name} </p>
+    </>
   )
   }
 
